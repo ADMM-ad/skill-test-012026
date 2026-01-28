23e264c15ee80e235aba4727022fd588c85f6292
ADMM-ad <adamabdurrahman514@gmail.com>
2026-01-28 10:31:20 +0700

    Add request untuk post

---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['sometimes', 'string', 'max:255'],
+            'content' => ['sometimes', 'string'],
+            'is_draft' => ['sometimes', 'boolean'],
+            'published_at' => ['nullable', 'date'],
+        ];
+    }
+}
